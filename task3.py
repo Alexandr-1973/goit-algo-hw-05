22a25f4074f8590c0f0960af744e748a6653f7a1
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2024-11-09 01:34:42 +0200

    task3-add

--- task3.py
+++ task3.py
@@ -1,5 +1,6 @@
 import sys
-
+from collections import defaultdict
+from colorama import Fore
 
 def parse_log_line(line: str) -> dict:
     log_list=line.split(" ")
@@ -29,14 +30,35 @@ def filter_logs_by_level(logs: list, level: str) -> list:
     return list(filter(lambda log:log["log_level"]==level, logs))
 
 def count_logs_by_level(logs: list) -> dict:
-    pass
+    count_level_dict=defaultdict(int)
+    for log in logs:
+        count_level_dict[log["log_level"]]+=1
+    return count_level_dict
 
-def display_log_counts(counts: dict):
-    pass
-
+def display_log_counts(counts: dict, level_value="ERROR"):
+    if len(counts):
+        print("\nРівень логування | Кількість\n"
+              "-----------------|----------")
+        for k,v in counts.items():
+            print(f"{Fore.RED+k+Fore.RESET if k==level_value else k}{' '*(17 - len(k))}| {v}")
+        print(" ")
 
 def main():
-    print(filter_logs_by_level(load_logs("log.txt"), "INFO"))
-    # print(sys.argv)
+    if len(sys.argv)>2:
+        level_value=sys.argv[2].upper()
+        logs_list=load_logs(sys.argv[1])
+        if not logs_list:
+            return
+        display_log_counts(count_logs_by_level(logs_list),level_value)
+        filter_list = filter_logs_by_level(logs_list, level_value)
+        if not filter_list:
+            print("Level not found")
+            return
+        print(f"Деталі логів для рівня '{level_value}':")
+        for log in filter_logs_by_level(logs_list,level_value):
+            print(f"{log["log_date"]} {log["log_time"]} - {log["message"].removeprefix(f"{level_value} ")}")
+        print(" ")
+    else:
+        display_log_counts(count_logs_by_level(load_logs(sys.argv[1])))
 
 main()
